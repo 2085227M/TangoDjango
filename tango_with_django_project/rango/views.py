a3f95c3a823fb491bba0c5f9867fcb2573e8d879
2085227M <2085227M@student.gla.ac.uk>
2018-01-18 17:01:30 +0000

    complete to end of chapter 4.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -13,4 +13,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <br/> <a href="/rango/">Index</a>')
+    context_dict = {'boldmessage': "Johnny Depp played Rango the Chameleon in the movie, here he is in character"}
+    return render(request, 'rango/about.html', context=context_dict)
