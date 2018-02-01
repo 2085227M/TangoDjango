2e691e1b0397862b97e2be94678171220e934b7d
2085227M <2085227M@student.gla.ac.uk>
2018-02-01 15:27:29 +0000

    swapped back to student email account

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,10 +8,9 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
 
     page_list = Page.objects.order_by('-views')[:5]
-
-    context_dict['pages'] = pages
-    context_dict['category'] = category
-    #context_dict = {'categories': category_list, 'pages': page_list}
+    context_dict = {'categories': category_list, 'pages': page_list}
+    #context_dict['pages'] = pages
+    #context_dict['category'] = category
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
