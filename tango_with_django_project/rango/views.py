ae9d1e2f7c4496b543ae9b0853f2df84bdacf5c3
2085227M <2085227M@student.gla.ac.uk>
2018-02-01 17:28:16 +0000

    Chapter 7 to bottom of exercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
@@ -32,3 +33,20 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        #Have we been provided a valid form?
+        if form.is_valid():
+            #Save the new category to the database
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form})
