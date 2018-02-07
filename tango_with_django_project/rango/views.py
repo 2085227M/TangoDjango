c7e078fecdef1c51af43d859af6de72622b81c64
2085227M <2085227M@student.gla.ac.uk>
2018-02-07 22:57:56 +0000

    complete through 10.5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,9 +8,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        visits = 1
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
 
 def index(request):
     #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by('-likes')[:5]
 
     page_list = Page.objects.order_by('-views')[:5]
@@ -21,9 +39,15 @@ def index(request):
 
     response = render(request, 'rango/index.html', context=context_dict)
 
+    visitor_cookie_handler(request, response)
+
     return response
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
